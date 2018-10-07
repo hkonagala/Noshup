adca95d2a8717f8e5b53ccd95815fe6464f44f6c
Harika Konagala <harika_konagala@Georges-MacBook-Pro-2.local>
2018-10-07 00:24:27 -0400

    cardview item added

--- app/src/main/java/homeaway/com/foodfinder/SearchActivity.java
+++ app/src/main/java/homeaway/com/foodfinder/SearchActivity.java
@@ -3,13 +3,27 @@ package homeaway.com.foodfinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class SearchActivity extends AppCompatActivity {
 
     FloatingSearchView searchView;
     RecyclerView restaurantsList;
+
+    public static final String CLIENT_ID = "5T2VIFHWWPITXXDVGVZVOXJPPIGR2WV1QLUSSP5URFLGQ2DS";
+    public static final String CLIENT_SECRET = "TPTIFH52P3Y0ZR3F0CP1FUQCER5KZR5XW4GF1WPJY50GLJXE";
+    private static String DEFAULT_QUERY = "restaurants";
+    private static String DATE;
+    public static final String SEARCH_API = "https://api.foursquare.com/v2/venues/search" +
+            "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&near=Seattle,+WA" +
+            "&query=" + DEFAULT_QUERY + "&v=" + DATE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,8 @@ public class SearchActivity extends AppCompatActivity {
 
         searchView = findViewById(R.id.floating_search_view);
         restaurantsList = findViewById(R.id.search_rv);
+
+        DATE = getTodayDate();
 
     }
 
@@ -49,4 +65,13 @@ public class SearchActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
+
+    /***************************************helper methods************************************/
+
+    public String getTodayDate(){
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("yyyyMMdd", Locale.US);
+        return mdformat.format(calendar.getTime());
+    }
+
 }
