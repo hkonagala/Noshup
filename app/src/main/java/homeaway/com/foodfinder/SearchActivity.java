fa5e149a11ce5a2170ac844581e49054a86bed63
Harika Konagala <harika_konagala@Georges-MacBook-Pro-2.local>
2018-10-06 21:50:17 -0400

    floating toolbar

--- app/src/main/java/homeaway/com/foodfinder/SearchActivity.java
+++ app/src/main/java/homeaway/com/foodfinder/SearchActivity.java
@@ -2,12 +2,51 @@ package homeaway.com.foodfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.arlib.floatingsearchview.FloatingSearchView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    FloatingSearchView searchView;
+    RecyclerView restaurantsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchView = findViewById(R.id.floating_search_view);
+        restaurantsList = findViewById(R.id.search_rv);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        searchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+            @Override
+            public void onSearchTextChanged(String oldQuery, String newQuery) {
+                //do something
+            }
+        });
+
+        searchView.setOnLeftMenuClickListener(new FloatingSearchView.OnLeftMenuClickListener() {
+            @Override
+            public void onMenuOpened() {
+                //do something
+            }
+
+            @Override
+            public void onMenuClosed() {
+                //do something
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
